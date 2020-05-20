92a9538e06d78842bb5022921b3f3eba02ab33eb
Shadman <shadmanchowdhury87@gmail.com>
2020-05-20 23:25:16 +0600

    Permutation in String added

---
+++ PermutationInString.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkInclusion(string p, string s) {
+        vector <int> v1(26, 0), v2(26, 0);
+        if(s.size() < p.size())
+            return false;
+        for(int i = 0; i < p.length(); i++) {
+            v1[s[i] - 'a']++;
+            v2[p[i] - 'a']++;
+        }
+        if(v1 == v2) {
+            return true;
+        }
+        for(int i = p.length(); i < s.length(); i++) {
+            v1[s[i] - 'a']++;
+            v1[s[i - p.length()] - 'a']--;
+            if(v1 == v2)
+                return true;
+        }
+        return false;
+    }
+};
