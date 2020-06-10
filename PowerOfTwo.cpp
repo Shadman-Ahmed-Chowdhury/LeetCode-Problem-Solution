c39937fae3d6fb771580fab344c78d8829ac4ec6
Shadman <shadmanchwodhury87@gmail.com>
2020-06-10 17:51:54 +0600

    Power Of Two Problem added

---
+++ PowerOfTwo.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n == 0)
+            return false;
+        if(n == 1)
+            return true;
+
+        while(n % 2 == 0) {
+            n = n >> 1;
+            //cout << n << endl;
+            if(n == 1)
+                return true;
+        }
+        return false;
+
+    }
+};
