5d68e96a7ec1c9a9a745951e0eec25a925fb745a
Shadman <shadmanchowdhury87@gmail.com>
2020-06-25 15:14:08 +0600

    Find Duplicate Number Problem added

---
+++ FindTheDuplicateNumber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        map<int, int> mp;
+        for(int n : nums) {
+            mp[n]++;
+            if(mp[n] > 1)
+                return n;
+        }
+        return -1;
+    }
+};
